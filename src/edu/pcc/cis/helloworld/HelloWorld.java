f03c3819b3e442e20732e88258dd196f5d22e808
Mike Torres <mike.torres@pcc.edu>
2017-07-23 21:32:38 -0700

    added main method that prints hello world

---
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.pcc.cis.helloworld;
+
+/**
+ *  Created by Mike Torres on  7/23/17
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
